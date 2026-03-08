386ab4f1e7c4ae20bc774e924a5b20bd806d43fd
Romero <romio04pro@gmail.com>
2026-03-08 19:28:31 +0200

    Laba3

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Post from "./components/molecules/Post/Post";
+import { postsData } from "./data";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.appContainer}>
+      <h1 style={{ textAlign: "center" }}>Стрічка новин</h1>
+
+      <div className={styles.feed}>
+        {postsData.map((post) => (
+          <Post
+            key={post.id} 
+            author={post.author}
+            content={post.content}
+            date={post.date}
+            avatar={post.avatar}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
